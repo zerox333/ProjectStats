472575b6356bc51a88e7b0e72ccb1b80e9778a9b
zerox333 <lawliet0914@gmail.com>
2013-04-11 22:24:33 +0800

    Initial Commit

---
+++ ProjectStats/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ProjectStats
+//
+//  Created by ZeroX on 13-4-11.
+//  Copyright (c) 2013年 ZeroX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
